18a8db8e90a2c87109e2e137955655076c88c3cd
Tim Diels <tim@timdiels.be>
2013-05-02 21:02:23 +0200

    Also print log messages rather than just writing

--- lim_os/include/core/log.lua
+++ lim_os/include/core/log.lua
@@ -3,4 +3,5 @@ local f = fs.open("error.log", 'a')
 function log(message)
 	f.writeLine('Day '..os.day()..' '..textutils.formatTime( os.time(), true )..': '..message)
 	f.flush()
+	print(message)
 end
